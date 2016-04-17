760acc6ef68ad78041d5c0dd2ae57792a7de47af
Ramya <ramya@mapbox.com>
2016-04-17 17:28:27 +0530

    clusters for places

--- map.js
+++ map.js
@@ -1,3 +1,6 @@
+'use strict';
+var placesJSON = require('./places.json');
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ1bmFzYW5rIiwiYSI6ImRKNlNQa3MifQ.SIx-g-J1oWWlP4grTXopcg';
 var map = new mapboxgl.Map({
     container: 'map', // container id
@@ -6,25 +9,88 @@ var map = new mapboxgl.Map({
     zoom: 4 // starting zoom
 });
 
+var places = new mapboxgl.GeoJSONSource({
+    'type': 'geojson',
+    'data': placesJSON,
+    'cluster': true,
+    'clusterMaxZoom': 14, // Max zoom to cluster points on
+    'clusterRadius': 50
+});
 map.on('style.load', function(){
 
-	var collegesSource = $.getJSON("colleges.geojson", function(json) {
-    	console.log(json);
-	});
+	map.addSource('places', places);
 
-    // map.addSource("colleges", {
-    //     type: "geojson",
-    //     data: collegesSource
-    // });
+    map.addLayer({
+        'id': 'non-cluster-places',
+        'type': 'circle',
+        'source': 'places',
+        'interactive': true,
+        'layout': {
+            visibility: 'visible'
+        },
+        'paint': {
+            'circle-radius': 14,
+            'circle-color': '#f1f075',
+            'circle-blur' : .9
+        }
+    });
 
-    // map.addLayer({
-    //     "id": "colleges",
-    //     "type": "symbol",
-    //     "source": "colleges",
-    //     "layout": {
-    //         "icon-image": "marker-15"
-    //     }
-    // });
+    var layers = [
+        [4, '#e62749'],
+        [2, '#e62749'],
+        [0, '#e62749']
+    ];
+    layers.forEach(function (layer, i) {
+        map.addLayer({
+            "id": "cluster-" + i,
+            "type": "circle",
+            "source": "places",
+            "paint": {
+                "circle-color": layer[1],
+                "circle-radius": 18,
+                "circle-blur": 0.9
+            },
+            "filter": i == 0 ?
+                [">=", "point_count", layer[0]] :
+                ["all",
+                    [">=", "point_count", layer[0]],
+                    ["<", "point_count", layers[i - 1][0]]]
+        });
+    });
+    map.addLayer({
+        "id": "cluster-count",
+        "type": "symbol",
+        "source": "places",
+        "layout": {
+            "text-field": "{point_count}",
+            "text-font": [
+                    "DIN Offc Pro Medium",
+                    "Arial Unicode MS Bold"
+                ],
+            "text-size": 12
+        }
+    });
+
 });
+
+
+
+map.on('click', function (e) {
+    var features = map.queryRenderedFeatures(e.point, { layers: ['places'] });
+
+    if (!features.length) {
+        return;
+    }
+
+    var feature = features[0];
+
+    // Populate the popup and set its coordinates
+    // based on the feature found.
+    var popup = new mapboxgl.Popup()
+        .setLngLat(feature.geometry.coordinates)
+        .setHTML(feature.properties.scientist)
+        .addTo(map);
+});
+
 
 
