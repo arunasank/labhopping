0bc2a929b28dfcd5db06e2e15706fe94b53ee544
arunasank <aruna@mapbox.com>
2016-04-17 20:56:33 +0530

    New build.js html

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+    
+    (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 var placesJSON = require('./places.json');
 
@@ -19,7 +20,7 @@ var places = new mapboxgl.GeoJSONSource({
 });
 map.on('style.load', function () {
 
-	map.addSource('places', places);
+    map.addSource('places', places);
 
     map.addLayer({
         'id': 'non-cluster-places',
@@ -90,9 +91,11 @@ map.on('click', function (e) {
     // based on the feature found.
     if (feature.properties.scientist) {
         console.log(JSON.stringify(feature));
+        var url = feature.properties.url;
+        console.log(url);
         var popup = new mapboxgl.Popup()
             .setLngLat(feature.geometry.coordinates)
-            .setHTML(feature.properties.scientist)
+            .setHTML("<a target='_blank' href=" + url + ">"+ feature.properties.scientist +"</a>" )
             .addTo(map);
     }
     else {
@@ -256,8 +259,8 @@ module.exports={
   "geometry": {
     "type": "Point",
     "coordinates": [
-      76.9574,
-      8.5058
+      76.2535,
+      9.9472
     ]
   }
 }
